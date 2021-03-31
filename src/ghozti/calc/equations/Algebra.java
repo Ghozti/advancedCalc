b0081c381f3e432982c2e0d61fc61d15ecc2036d
Ghozti <ybalderas1005@gmail.com>
2021-03-31 16:14:36 -0400

    - got halfway done through point slope

--- src/ghozti/calc/equations/Algebra.java
+++ src/ghozti/calc/equations/Algebra.java
@@ -5,4 +5,10 @@ public class Algebra {
     public static double slope(float x1, float x2, float y1, float y2){
         return (y2 - y1)/(x2 - x1);
     }
+
+    public static String pointSlope(double x1, double y1, double m){
+        //y - y1 = m(x + x1)
+        String step1 = m + "x" + (m*x1 > 0 ? "+" : "-") + m * x1;
+        return "";
+    }
 }
--- src/ghozti/calc/equations/BasicEquations.java
+++ src/ghozti/calc/equations/BasicEquations.java
@@ -13,4 +13,8 @@ public class BasicEquations {
     public static double divide(double num1, double num2){
         return num1 / num2;
     }
+
+    public static double multiply(double num1, double num2){
+        return num1*num2;
+    }
 }
