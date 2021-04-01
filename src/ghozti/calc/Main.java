04da3a48de25e7c99a7a303c8efaa4a1d606341f
Ghozti <ybalderas1005@gmail.com>
2021-04-01 12:50:41 -0400

    - finished distance in Geo.java

--- src/ghozti/calc/Main.java
+++ src/ghozti/calc/Main.java
@@ -1,10 +1,11 @@
 package ghozti.calc;
 
 import ghozti.calc.equations.Algebra;
+import ghozti.calc.equations.Geo;
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(Algebra.pointSlope(2,10,-3));
+        System.out.println(Geo.distance(6,0,8,0));
     }
 }
--- src/ghozti/calc/equations/Geo.java
+++ src/ghozti/calc/equations/Geo.java
@@ -1,4 +1,12 @@
 package ghozti.calc.equations;
 
+import ghozti.calc.Main;
+
 public class Geo {
+
+    public static final String distance(double x2, double x1, double y2, double y1){
+        double first = Math.pow((x2 - x1),2);
+        double second = Math.pow((y2 - y1),2);
+        return "d = ".concat(Double.toString(Math.sqrt(first + second)));
+    }
 }
