13994272190d9b3ec53b859392623906a11f0cf2
Ghozti <ybalderas1005@gmail.com>
2021-04-01 12:43:40 -0400

    - finished Algebra.java for now

--- src/ghozti/calc/equations/Algebra.java
+++ src/ghozti/calc/equations/Algebra.java
@@ -7,13 +7,17 @@ public class Algebra {
     }
 
     public static String pointSlope(double x1, double y1, double m){
-
+        //mx
         String mx = m + "x";
-
+        //mx1
         double mx1 = m * x1;
-
+        //y1 * mx1
         double y1Mx1 = (y1 > 0 ? y1 - mx1 : y1 + mx1);
-
+        //final result
         return "y = ".concat(mx).concat((y1Mx1 > 0 ? " + " : " - ")).concat(String.valueOf(y1Mx1));
+    }
+
+    public static double slope(double y2, double y1, double x2, double x1){
+        return (y2 - y1) / (x2 - x1);
     }
 }
