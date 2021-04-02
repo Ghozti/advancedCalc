365753834165015ead8491954ad16f71c00221b0
Ghozti <ybalderas1005@gmail.com>
2021-04-02 14:42:13 -0400

    - added missingSide in Geo

--- src/ghozti/calc/equations/Geo.java
+++ src/ghozti/calc/equations/Geo.java
@@ -4,9 +4,13 @@ import ghozti.calc.Main;
 
 public class Geo {
 
-    public static final String distance(double x2, double x1, double y2, double y1){
+    public static String distance(double x2, double x1, double y2, double y1){
         double first = Math.pow((x2 - x1),2);
         double second = Math.pow((y2 - y1),2);
         return "d = ".concat(Double.toString(Math.sqrt(first + second)));
+    }
+
+    public static String missingSide(double a, double b){
+        return "missing side = ".concat(String.valueOf(180 - (a+b)));
     }
 }
