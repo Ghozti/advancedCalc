2f0bb6b3e18a507ae286e95eacdf9d31f6545552
Ghozti <ybalderas1005@gmail.com>
2021-04-02 22:04:23 -0400

    - moved the triangle to a final variable in Geo.java

--- src/ghozti/calc/equations/Geo.java
+++ src/ghozti/calc/equations/Geo.java
@@ -2,6 +2,18 @@ package ghozti.calc.equations;
 
 public class Geo {
 
+    private static final String triangle1 =
+            "  |\\\n" +
+            "  | \\\n" +
+            "  |  \\\n" +
+            "  |   \\\n" +
+            "a |    \\ c\n" +
+            "  |     \\\n" +
+            "  |      \\\n" +
+            "  |       \\\n" +
+            "  |________\\\n" +
+            "      b";
+
     public static String distance(double x2, double x1, double y2, double y1){
         double first = Math.pow((x2 - x1),2);
         double second = Math.pow((y2 - y1),2);
@@ -13,16 +25,7 @@ public class Geo {
     }
 
     public static String cos(){
-        System.out.println( "  |\\\n" +
-                            "  | \\\n" +
-                            "  |  \\\n" +
-                            "  |   \\\n" +
-                            "a |    \\ c\n" +
-                            "  |     \\\n" +
-                            "  |      \\\n" +
-                            "  |       \\\n" +
-                            "  |________\\\n" +
-                            "   b");
+        System.out.println(triangle1);
         return "";
     }
 }
